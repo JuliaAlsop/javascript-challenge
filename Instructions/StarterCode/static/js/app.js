065ea9f03a9e369c81de2c991e26a42affe87fef
Julia Alsop <64661590+JuliaAlsop@users.noreply.github.com>
2020-09-13 17:22:16 -0400

    Addition of table upon webpage loading

--- Instructions/StarterCode/static/js/app.js
+++ Instructions/StarterCode/static/js/app.js
@@ -2,3 +2,27 @@
 var tableData = data;
 
 // YOUR CODE HERE!
+var tbody = d3.select("tbody");
+
+console.log(data);
+
+data.forEach(element =>{
+  console.log(element)
+})
+
+data.forEach(element =>{
+  console.log(Object.entries(element))
+});
+
+data.forEach(element =>{
+  let row = tbody.append("tr");
+  row.append("td").text(element.datetime);
+  row.append("td").text(element.city);
+  row.append("td").text(element.state);
+  row.append("td").text(element.country);
+  row.append("td").text(element.shape);
+  row.append("td").text(element.durationMinutes);
+  row.append("td").text(element.comments);
+})
+
+d3.select("table").attr("class", "table table-striped table-hover");
